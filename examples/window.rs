c7f39865a2c8fb7a43ef737a57cdf6b7137e8fee
Tim Anderegg <timothy.anderegg@gmail.com>
2016-12-11 19:37:04 -0500

    Remove explicit settings that are no longer needed.

--- examples/window.rs
+++ examples/window.rs
@@ -11,11 +11,6 @@ fn resize_callback(width: u32, height: u32) {
 
 fn main() {
     let mut window = glutin::WindowBuilder::new()
-    .with_pixel_format(24, 8)
-    .with_depth_buffer(24)
-    .with_stencil_buffer(8)
-    .with_gl(GlRequest::Specific(glutin::Api::OpenGl, (4, 5)))
-    .with_gl_profile(GlProfile::Core)
     .build().unwrap();
     window.set_title("A fantastic window!");
     window.set_window_resize_callback(Some(resize_callback as fn(u32, u32)));
